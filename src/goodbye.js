1c536403370c6acbabd8a2c8f17f3fc7a38f4d23
Moataz <moataz.mohammady@gmail.com>
2019-06-24 17:35:13 +0200

    Styling

--- src/goodbye.js
+++ src/goodbye.js
@@ -8,6 +8,10 @@ export default class Goodbye extends Component {
         this.state = {clicks: 0}; // state should be initialized here
     }
 
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
     handleClick() {
         // Do not mutate state directly. Use setState()  react/no-direct-mutation-state
         // this.setState({clicks: ++this.state.clicks}); // state should be updated like this
@@ -18,12 +22,20 @@ export default class Goodbye extends Component {
 
     render() {
         // this.props.name = 'Islam'; //TypeError: Cannot assign to read only property 'name' of object '#<Object>'
-        const {name} = this.props;
+        const {name, age} = this.props;
         const {clicks} = this.state;
-        return <h1 onClick={() => this.handleClick()}>Goodbye, {name} {clicks} time(s)!</h1>;
+
+        // this.props.age = 80; //TypeError: Cannot assign to read only property 'age' of object '#<Object>'
+
+        return <div>
+            <button onClick={() => this.handleClick()} className="button-success pure-button">Say Goodbye</button>
+            <h1>Goodbye,{clicks} time(s)!</h1>
+            <h2>You Name is {name}</h2>
+            <h3>You have {age} years old</h3>
+        </div>;
     }
 }
-Goodbye.defaultProps = {name: 'Foo'};
+Goodbye.defaultProps = {name: 'Foo', age: 18};
 Goodbye.propTypes = {name: PropTypes.string.isRequired};
 
 
